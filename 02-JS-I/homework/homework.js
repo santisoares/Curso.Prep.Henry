7f77ad0654957316a4e000d96d87139bea394854
unknown <santisoaresgache@gmail.com>
2021-03-23 18:11:40 -0300

    arreglos en js2

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -60,7 +60,7 @@ function sonIguales(x, y) {
   // Devuelve "true" si "x" e "y" son iguales
   // De lo contrario, devuelve "false"
   // Tu código: 
-  if (x == y) {
+  if (x === y) {
     return true;
   } else {
     return false;
@@ -71,7 +71,7 @@ function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
-  return str1.length == str2.length ;
+  return str1.length === str2.length ;
 } 
 
 function menosQueNoventa(num) { 
